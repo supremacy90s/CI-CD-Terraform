3df9f19da269049ac5fc206f0c362a9f1aeabfc3
Matsya <132735749+supremacy90s@users.noreply.github.com>
2023-12-12 12:41:23 +0530

    Create Node.js

---
+++ Node.js
@@ -0,0 +1,13 @@
+// app.js
+
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello, CI/CD!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
